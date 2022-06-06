ee77abf5f2c22a72c99b7815fbf8879041d7aa0e
Ajay Kumar <ajaykumarmail@gmail.com>
2022-06-06 12:22:46 +0530

    guterberg https url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,5 @@
-const BASE_URL = 'http://skunkworks.ignitesol.com:8000';
+// const BASE_URL = 'http://skunkworks.ignitesol.com:8000';
+
+const BASE_URL = 'https://gutendex.com';
 
 export { BASE_URL };
