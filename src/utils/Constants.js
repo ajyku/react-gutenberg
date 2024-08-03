2ad71ba59f73a652398ee3776abd82934519dba5
Ajay Kumar <ajaykumarmail@gmail.com>
2024-08-03 20:55:46 +0530

    fix: api url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://skunkworks.ignitesol.com:8000';
+// const BASE_URL = 'http://skunkworks.ignitesol.com:8000';
 
-// const BASE_URL = 'https://gutendex.com';
+const BASE_URL = 'https://gutendex.com';
 
 export { BASE_URL };
